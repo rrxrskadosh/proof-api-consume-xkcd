46f452600b06a8fee53b4d438aa344603d5e9a83
rrxrskadosh <kadoshrr2019@gmail.com>
2022-05-13 16:23:25 -0500

    Created Component Start Rating

--- src/components/StarRating.vue
+++ src/components/StarRating.vue
@@ -1,12 +1,107 @@
 <template>
   <p>Rating component</p>
+  <div class="star-rating">
+    <label class="star-rating__star" v-for="rating in ratings"
+    :class="{
+    'is-selected': ((value >= rating) && value != null), 'is-disabled': disabled}" 
+     v-on:click="set(rating)" v-on:mouseover="star_over(rating)" v-on:mouseout="star_out">
+    <input class="star-rating star-rating__checkbox" type="radio" :value="rating" :name="name"
+    v-model="value" :disabled="disabled">★</label>
+  </div>
 </template>
 
 
 <script>
 
+export default {
+  name: 'StarRating',
+  components: {},
+  props: {
+    'name': String,
+    'value': null,
+    'id': String,
+    'disabled': Boolean,
+    'required': Boolean
+  },
+  data: function() {
+    return {
+      temp_value: null,
+      ratings: [1, 2, 3, 4, 5]
+    };
+  },
+
+  methods: {
+    /* Behaviour of the stars on mouseover */
+    star_over: function(index) {
+      var self = this;
+
+      if (!this.disabled) {
+        this.temp_value = this.value;
+        return this.value = index;
+      }
+
+    },
+
+    /* Behaviour of the stars on mouseout */
+    star_out: function() {
+      var self = this;
+
+      if (!this.disabled) {
+        return this.value = this.temp_value;
+      }
+    },
+
+    /* Set the rating. */
+    set: function(value) {
+      var self = this;
+
+      if (!this.disabled) {
+        this.temp_value = value;
+        return this.value = value;
+      }
+    }
+  }
+}
+
 </script>
 
 <style lang="scss" scoped>
+
+%visually-hidden {
+  position: absolute;
+  overflow: hidden;
+  clip: rect(0 0 0 0);
+  height: 1px; width: 1px;
+  margin: -1px; padding: 0; border: 0;
+}
+
+.star-rating {
+
+  &__star {
+    display: inline-block;
+    padding: 5px;
+    vertical-align: middle;
+    line-height: 1;
+    font-size: 3em;
+    color: #ABABAB;
+    transition: color .2s ease-out;
+
+    &:hover {
+      cursor: pointer;
+    }
+    
+    &.is-selected {
+      color: #FFD700;
+    }
+    
+    &.is-disabled:hover {
+      cursor: default;
+    }
+  }
+
+  &__checkbox {
+    @extend %visually-hidden;
+  }
+}
 
 </style>
